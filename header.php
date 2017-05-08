24f3b22da116c2838507cf9f2a41d1d21ea5ff83
nataliewang919 <nataliewang919@kakoshaochies-MacBook-Pro.local>
2017-05-08 11:50:15 -0700

    update to make responsive drop down menu work

--- header.php
+++ header.php
@@ -3,18 +3,19 @@
 <head>
 <title><?php bloginfo('description'); ?> | <?php bloginfo('name'); ?></title>
 <meta charset="UTF-8" />
-<meta name="viewport" content="width=device-width" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
 <script src="https://s3.amazonaws.com/menumaker/menumaker.min.js" type="text/javascript"></script>
-<script src="js/script.js"></script>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">  
+<script src="<?php bloginfo('template_directory'); ?>/js/script.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"> 
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="all" />
 <link href="<?php bloginfo('template_directory'); ?>/flexslider.css" rel="stylesheet" media="screen" type="text/css" /> 
+    
     <!-- Start FlexSlider Code, place just before closing body tag --> 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/jquery.flexslider.js"></script> 
-
+  
 <!-- Initialize the slider on the div we named "flexslider"-->
 <script type="text/javascript">
     $(window).load(function(){
@@ -40,17 +41,6 @@
    </div>
  <h2 class="pageid">Welcome to Peking Fun</h2>
   <h1>Peking Fun</h1>
-  <!-- START NAVIGATION (Main)
-<div id="cssmenu">
-  <ul>
- <li class="active"><a href="index.php"><span>Main Page</span></a></li>
-     <li><a href="index.php"><span>About</span></a></li>
-     <li><a href="index.php"><span>Protfolio</span></a></li>     
-     <li><a href="index.php"><span>Blog</span></a></li>
-     <li><a href="index.php"><span>Contact</span></a></li>
-    </ul>
-  </div>
-     END NAVIGATION (Main) -->  
    
 <!-- START Wordpress NAVIGATION (Main) -->
 

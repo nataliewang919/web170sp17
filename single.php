bf263906456a904a32e7e1fbf7e0eb1e69068db4
nataliewang919 <nataliewang919@kakoshaochies-MacBook-Pro.local>
2017-05-09 18:44:41 -0700

    update to show single post category

--- single.php
+++ single.php
@@ -5,8 +5,11 @@
 <article class=mainpage>
 
 <?php if (have_posts() ): while (have_posts() ) : the_post(); ?>
-<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>  
+<article id="article-<?php the_ID(); ?>" class="article">
+<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+<small>Posted on <?php the_time('F j, Y'); ?> by <?php the_author(); ?> in <?php the_category(); ?></small>
 <?php the_content(); ?>
+    </article>
 <?php endwhile; endif; ?>
     <small>single.php</small>
 </article>

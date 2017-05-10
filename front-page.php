54fdee1a2fefdee966bdbc5a76beb96535d17812
nataliewang919 <nataliewang919@kakoshaochies-MacBook-Pro.local>
2017-05-10 14:48:32 -0700

    update to make front page latest news work

--- front-page.php
+++ front-page.php
@@ -19,17 +19,37 @@
 <!--start contents-->
 <div class="mainpartone">
 <img alt="" src="<?php bloginfo('template_directory'); ?>/images/tiantan1.jpg">
-<p>I love cheese, especially everyone loves cheese and biscuits. Cream cheese cheesy grin taleggio macaroni cheese port-salut brie ricotta say cheese. Cauliflower cheese goat halloumi everyone loves cut the cheese halloumi squirty cheese cut the cheese. Mascarpone everyone loves red leicester blue castello paneer cheese slices fromage frais squirty cheese. Cheddar manchego say cheese.</p>
+    <h2>About Me</h2>
+<?php if (have_posts() ): while (have_posts() ) : the_post(); ?>
+<article id="article-<?php the_ID(); ?>" class="article">
+    <?php the_content(); ?>
+</article>
+<?php endwhile; endif; ?>
+     <small>front-page.php</small>
 
 </div>
 
 <div class="mainparttwo">
 <img alt="" src="<?php bloginfo('template_directory'); ?>/images/greatwallnewmeitu.jpg">
-<p>I love cheese, especially everyone loves cheese and biscuits. Cream cheese cheesy grin taleggio macaroni cheese port-salut brie ricotta say cheese. Cauliflower cheese goat halloumi everyone loves cut the cheese halloumi squirty cheese cut the cheese. Mascarpone everyone loves red leicester blue castello paneer cheese slices fromage frais squirty cheese. Cheddar manchego say cheese.</p>
-    
+<h2>Latest News</h2>
+    <ul>
+<?php rewind_posts(); ?>
+<?php query_posts(array('posts_per_page'=>'4','category_name'=>'news')); ?>
+<?php if (have_posts() ): while (have_posts() ) : the_post(); ?>
+<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><br><small><?php the_time('F jS, Y') ?> in <?php the_category(', ') ?></small> 
+<?php endwhile; endif; ?>  
+  </ul>
+   
+<h2>Latest Article</h2>
+    <ul>
+<?php rewind_posts(); ?>
+<?php query_posts(array('posts_per_page'=>'4','category_name'=>'articles')); ?>
+<?php if (have_posts() ): while (have_posts() ) : the_post(); ?>
+<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><br><small><?php the_time('F jS, Y') ?> in <?php the_category(', ') ?></small><br />     
+<?php endwhile; endif; ?>  
+    </ul>
 </div>
 <!-- end main contents --> 
 
-<small>front-page.php</small>
 
 <?php get_footer(); ?>

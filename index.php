32b53e05e7a8c88e1d9c0d64136e1dd9af218231
nataliewang919 <nataliewang919@kakoshaochies-MacBook-Pro.local>
2017-05-19 17:09:22 -0700

    fix css for Featured Images

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php get_header(); ?>
 
 <!-- start main contents -->
-<article class=mainpage>
+<article class="mainpage">
 <?php if (have_posts() ): while (have_posts() ) : the_post(); ?>
 <article id="article-<?php the_ID(); ?>" class="article">
-<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+<h2 class="blogtitle"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 <small><?php the_time('F j, Y'); ?> by <?php the_author(); ?> in <?php the_category(', '); ?></small>
 <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
-<p><?php echo get_the_excerpt(); ?><br /><a href="<?php the_permalink(); ?>">Full Story&nbsp;&raquo;</a></p>
+<p><?php echo get_the_excerpt(); ?><a href="<?php the_permalink(); ?>">Full Story&nbsp;&raquo;</a></p>
 </article>
 <?php endwhile; endif; ?>
     <small>index.php</small>
